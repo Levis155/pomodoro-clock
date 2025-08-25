9c798caaad726a08536742ab0d74777b8bcfafe1
Levis155 <levismbui@gmail.com>
2025-08-25 05:53:02 +0300

    place elements on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div>
-      Hello world!
+    <div className="app-wrapper">
+      <h1>pomodoro clock</h1>
+      <div className="timer-length-btns">
+        <div className="break-length">
+          <span>break length</span>
+          <div>
+            <button>-</button>
+            <div>5</div>
+            <button>+</button>
+          </div>
+        </div>
+
+        <div className="session-length">
+          <span>break length</span>
+          <div>
+            <button>-</button>
+            <div>5</div>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <span>session</span>
+        <span>24:56</span>
+      </div>
+
+      <div>
+        <button>stop</button>
+        <button>reset</button>
+      </div>
+
+      <span>Designed and coded by <a href="https://www.linkedin.com/in/levis-mbui/">Levis Mbui</a></span>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
