019510b81f1b2fc05ef12bf14bce40ac7ae846b1
Levis155 <levismbui@gmail.com>
2025-08-26 06:09:25 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,43 @@ import "./App.css";
 function App() {
   return (
     <div className="app-wrapper">
-      <h1>pomodoro clock</h1>
+      <div className="app-title">
+        <hr />
+        <h1>pomodoro clock</h1>
+        <hr />
+      </div>
+      
       <div className="timer-length-btns">
         <div className="break-length">
           <span>break length</span>
-          <div>
+          <div className="increment-decrement-btns">
             <button>-</button>
-            <div>5</div>
+            <div className="length-display">5</div>
             <button>+</button>
           </div>
         </div>
 
         <div className="session-length">
-          <span>break length</span>
-          <div>
+          <span>session length</span>
+          <div className="increment-decrement-btns">
             <button>-</button>
-            <div>5</div>
+            <div className="length-display">5</div>
             <button>+</button>
           </div>
         </div>
       </div>
 
-      <div>
-        <span>session</span>
-        <span>24:56</span>
+      <div className="timer">
+        <span className="timer-title">session</span>
+        <span className="timer-countdown">24:56</span>
       </div>
 
-      <div>
+      <div className="stop-reset-btns">
         <button>stop</button>
         <button>reset</button>
       </div>
 
-      <span>Designed and coded by <a href="https://www.linkedin.com/in/levis-mbui/">Levis Mbui</a></span>
+      <span className="dev-info">Designed and coded by <a href="https://www.linkedin.com/in/levis-mbui/" target="_blank">Levis Mbui</a></span>
     </div>
   );
 }
